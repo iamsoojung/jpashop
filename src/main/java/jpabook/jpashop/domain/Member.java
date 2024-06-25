5859ce3336d013f6f22b3d2ad3c7086415fc634d
김수정 <crystar2402@naver.com>
2024-06-26 01:51:03 +0900

    add: 회원, 주문, 상품, 배송, 카테고리 entity 추가

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order의 member에 의해 매핑 (읽기 전용)
+    private List<Order> orders = new ArrayList<>();
+}
