f0549c000f8041b619a484ed8b2da641888a4396
iamsoojung <crystar2402@naver.com>
2024-08-25 19:49:27 +0900

    add: 주문 entity, 주문상품 entity 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,28 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
 
     private int count;  // 주문 수량
+
+    //==생성 메서드==//
+    // 주문상품, 가격, 수량 정보 사용해서 주문상품 엔티티 생성함
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);    // 주문수량 만큼 상품재고 줄임
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
