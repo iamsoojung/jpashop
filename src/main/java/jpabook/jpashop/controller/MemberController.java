fbf119c3fe0a99b8cf65347e773fc17e93d5725f
soojung <crystar2402@naver.com>
2024-12-27 11:44:04 +0900

    feat: 회원 등록 post method 추가

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.controller;
 
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,5 +21,24 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm()); // 뷰로 넘어갈 때 이 데이터를 실어서 넘김
         return "members/createMemberForm";  // 화면 정의
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result)  {   //  Form 을 필수로 쓰도록하기
+
+        // result에 error가 있다면 createMemberForm으로 보내겠음!
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
